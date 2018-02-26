f994e473b50ae933d9818995a3a3e7f93fe2fad8
obinnaeye <obinnaeye@yahoo.com>
2018-02-26 17:07:02 +0100

    test ussd

--- controllers/v1/ussd.controller.js
+++ controllers/v1/ussd.controller.js
@@ -27,8 +27,34 @@ export function generateJwt(data) {
  * @returns {object} the response
  */
 export async function ussd(req, res) {
-  console.log(req, 'REqueest ......')
+  const { sessionId, serviceCode, phoneNumber, text } = req.body;
+  console.log('ERERERE', req.body);
+  let response = '';
+  let accountNumber = '';
+  let balance = '';
+  if (text === '') {
+    // This is the first request. Note how we start the response with CON
+    response = 'CON What would you want to check \n 1. My Account \n 2. My phone number';
+  } else if (text === '1') {
+  // Business logic for first level response
+    response = 'CON Choose account information you want to view \n 1. Account number \n 2. Account balance';
+  } else if (text === '2') {
+  // Business logic for first level response
+  // This is a terminal request. Note how we start the response with END
+    response = 'END Your phone number is $phoneNumber';
+  } else if (text === '1*1') {
+    // This is a second level response where the user selected 1 in the first instance
+    accountNumber = 'ACC1001';
+    // This is a terminal request. Note how we start the response with END
+    response = `END Your account number is ${accountNumber}`;
+  } else if (text === '1*2') {
+    // This is a second level response where the user selected 1 in the first instance
+    balance = 'NGN 10,000';
+    // This is a terminal request. Note how we start the response with END
+    response = `END Your balance is ${balance}`;
+  }
 
-  return res.status(200).send('END Welcome: Your USSD is coming soon!');
+
+  return res.status(200).send(response);
 }
 
